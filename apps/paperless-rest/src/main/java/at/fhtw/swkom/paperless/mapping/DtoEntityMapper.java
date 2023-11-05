86b1d82526440e1f8ef3386ab579ade6f07a0d24
gawdkeno <if21b032@technikum-wien.at>
2023-11-05 16:53:44 +0100

    #11 alpha-map test (not working)

---
+++ apps/paperless-rest/src/main/java/at/fhtw/swkom/paperless/mapping/DtoEntityMapper.java
@@ -0,0 +1,37 @@
+package at.fhtw.swkom.paperless.mapping;
+
+import at.fhtw.swkom.paperless.persistence.entities.DocumentsDocument;
+import at.fhtw.swkom.paperless.persistence.entities.DocumentsDocumentTag;
+import at.fhtw.swkom.paperless.persistence.entities.Storagepath;
+import at.fhtw.swkom.paperless.persistence.entities.Tag;
+import at.fhtw.swkom.paperless.persistence.entities.Documenttype;
+import at.fhtw.swkom.paperless.services.dto.Correspondent;
+import at.fhtw.swkom.paperless.services.dto.DocTag;
+import at.fhtw.swkom.paperless.services.dto.Document;
+import at.fhtw.swkom.paperless.services.dto.NewTag;
+import org.mapstruct.Mapper;
+import org.openapitools.jackson.nullable.JsonNullable;
+
+@Mapper
+public interface DtoEntityMapper {
+    Document documentEntityToDto(DocumentsDocument documentsDocument);
+    DocumentsDocument documentDtoToEntity(Document document);
+    default JsonNullable<Integer> map(Correspondent correspondent) {
+        return correspondent!=null ? JsonNullable.of(Math.toIntExact(correspondent.getId())) : JsonNullable.undefined();
+    }
+    default JsonNullable<Integer> map(Documenttype documenttype) {
+        return documenttype!=null ? JsonNullable.of(documenttype.getId()) : JsonNullable.undefined();
+    }
+    default JsonNullable<Integer> map(Storagepath storagepath) {
+        return storagepath!=null ? JsonNullable.of(storagepath.getId()) : JsonNullable.undefined();
+    }
+    NewTag tagDtoToEntity(Tag tag);
+    Tag tagEntityToTag(NewTag newTag);
+//    DocTag docTagDtoToEntity(Tag tag);
+//    Tag docTagEntityToDto(DocTag docTag);
+    DocTag docTagDtoToEntity(DocumentsDocumentTag documentsDocumentTag);
+    DocumentsDocumentTag docTagEntityToDto(DocTag docTag);
+    Correspondent correspondentDtoToEntity(at.fhtw.swkom.paperless.persistence.entities.Correspondent correspondent);
+    at.fhtw.swkom.paperless.persistence.entities.Correspondent correspondentEntityToDto(Correspondent correspondent);
+
+}
---
+++ apps/paperless-rest/src/test/java/at/fhtw/skom/paperless/mapping/DocumentMappingTest.java
@@ -0,0 +1,95 @@
+package at.fhtw.skom.paperless.mapping;
+
+import at.fhtw.swkom.paperless.mapping.DtoEntityMapperImpl;
+import at.fhtw.swkom.paperless.persistence.entities.DocumentsDocument;
+import at.fhtw.swkom.paperless.mapping.DtoEntityMapper;
+//import at.fhtw.swkom.paperless.mapping.DtoEntityMapperImpl;
+import at.fhtw.swkom.paperless.services.dto.Document;
+import org.junit.jupiter.api.Test;
+import org.openapitools.jackson.nullable.JsonNullable;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class DocumentMappingTest {
+    @Test
+    void testDocumentEntityToDto () {
+        Integer id = 1;
+        Integer correspondent = 2;
+        Integer documentType = 3;
+        Integer storagePath = 4;
+        String title = "Title";
+        String content = "Content";
+        List<Integer> tags = List.of(100, 101);
+        OffsetDateTime created = OffsetDateTime.of(LocalDate.of(2030, 1, 1), LocalTime.of(3, 0, 0), ZoneOffset.ofHours(1));
+        OffsetDateTime createdDate = OffsetDateTime.of(LocalDate.of(2020, 1, 1), LocalTime.of(2, 0, 0), ZoneOffset.ofHours(1));
+        OffsetDateTime modified = OffsetDateTime.of(LocalDate.of(2010, 1, 1), LocalTime.of(1, 0, 0), ZoneOffset.ofHours(1));
+        OffsetDateTime added = OffsetDateTime.of(LocalDate.of(2000, 1, 1), LocalTime.of(0, 0, 0), ZoneOffset.ofHours(1));
+        String archiveSerialNumber = "5";
+        String originalFileName = "OriginalFileName";
+        String archivedFileName = "ArchivedFileName";
+
+        DocumentsDocument document = new DocumentsDocument();
+        document.setId(id);
+        document.setTitle(title);
+        document.setContent(content);
+        document.setCreated(created);
+        document.setModified(modified);
+        document.setAdded(added);
+        document.setArchiveSerialNumber(Integer.valueOf(archiveSerialNumber));
+        document.setArchiveFilename(archivedFileName);
+        // document.setOriginalFileName(originalFileName);
+
+        // TODO:
+        /*
+        String checksum
+        String storageType
+        String filename
+        String mimeType
+        String archiveChecksum
+        DocumentsCorrespondent correspondent
+        DocumentsDocumenttype documentType
+        DocumentsStoragepath storagePath
+        AuthUser owner
+        Set<DocumentsNote> documentDocumentsNotes
+        Set<DocumentsDocumentTags> documentDocumentsDocumentTagses
+        */
+
+        Document expectedDocumentDto = new Document();
+        expectedDocumentDto.setId(id);
+        expectedDocumentDto.setTitle(JsonNullable.of(title));
+        expectedDocumentDto.setContent(JsonNullable.of(content));
+        expectedDocumentDto.setCreated(created);
+        expectedDocumentDto.setModified(modified);
+        expectedDocumentDto.setAdded(added);
+        expectedDocumentDto.setArchiveSerialNumber(JsonNullable.of(archiveSerialNumber));
+        expectedDocumentDto.setArchivedFileName(JsonNullable.of(archivedFileName));
+        // expectedDocumentDto.setOriginalFileName(JsonNullable.of(originalFileName));
+
+        // TODO:
+        /*
+        expectedDocumentDto.setCorrespondent(JsonNullable.of(correspondent));
+        expectedDocumentDto.setDocumentType(JsonNullable.of(documentType));
+        expectedDocumentDto.setStoragePath(JsonNullable.of(storagePath));
+        expectedDocumentDto.setTags(JsonNullable.of(tags));
+        expectedDocumentDto.setCreatedDate(createdDate);
+         */
+
+        DtoEntityMapperImpl documentMapperImpl = new DtoEntityMapperImpl();
+        Document documentDto = documentMapperImpl.documentEntityToDto(document);
+
+        System.out.println("Expected -----------------");
+        System.out.println(expectedDocumentDto);
+
+        System.out.println("Actual -----------------");
+        System.out.println(documentDto);
+
+        assertEquals(expectedDocumentDto, documentDto);
+    }
+}
