5a5fef9f37cfaf71a527493ce6168bac1fcdf789
Martin <maschteigrubi@gmail.com>
2023-11-24 16:02:26 +0100

    [#27] extended compose, created ocr-module & Dockerfile

---
+++ apps/paperless-ocr/src/main/java/at/fhtw/swkom/paperless/PaperlessServicesApplication.java
@@ -0,0 +1,12 @@
+package at.fhtw.swkom.paperless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class PaperlessServicesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PaperlessServicesApplication.class, args);
+    }
+}
