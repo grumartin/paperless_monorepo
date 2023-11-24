b9ef3924355693624b511d68b7ceee5f6a33cd71
Martin <maschteigrubi@gmail.com>
2023-11-24 17:27:47 +0100

    [#30] added Tesseract to Dockerfile

--- apps/paperless-ocr/src/main/java/at/fhtw/swkom/paperless/PaperlessServicesApplication.java
+++ apps/paperless-ocr/src/main/java/at/fhtw/swkom/paperless/PaperlessServicesApplication.java
@@ -3,7 +3,6 @@ package at.fhtw.swkom.paperless;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class PaperlessServicesApplication {
     public static void main(String[] args) {
